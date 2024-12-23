d1ee24fe4906d9a105d4dabe0e866cc81eb216ac
wheee88 <wheee88@naver.com>
2024-12-23 17:40:07 +0900

    변수정의

---
+++ HESS.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <unistd.h> // POSIX 표준 sleep 함수를 위해
+
+double OnSig;
+double dmode;
+double hmode;
+double cmode;
+double mode;
+double V_disc;
+double V_ESS_BUS;
+double V_char;
+double SCES_Capa;
+double R_int;
+double SC_UL;
+double SC_LL;
+double BESS_Capa;
+double BE_UL;
+double BE_LL;
+double BESS_ramp;
+double BESS_ramp_lim;
+double P_RefB;
+double P_SUB;
+double P_ACLoad;
+double Ppeak;
+double Tlength;
+double Managed;
+double P_mng;
+double P_Sub;
+double P_SCES;
+double P_RefB;
+double P_Rail;
+double Vsrc_SCES;
+double Vsrc_BESS;
+double reset;
+
+
